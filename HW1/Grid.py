4329a36fb1824de7fd09a75bc9dc2db7200f2f18
Chris Seagraves <christopher.seagraves@gmail.com>
2022-08-24 12:52:10 -0500

    HW1 Complete

---
+++ HW1/Grid.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+
+from Node import Node
+
+################################################################################
+
+class Grid:
+    def __init__(self, max_x: int, max_y: int, grid_spacing: float) -> None:
+        self.max_x = max_x
+        self.max_y = max_y
+        self.grid_spacing = grid_spacing
+        self.nodes = self.get_nodes()
+
+        self.fig, self.ax = plt.subplots()
+        
+    ############################################################################
+
+    def get_nodes(self) -> list[Node]:
+        """
+        Creates a list of nodes in the grid
+        """
+        nodes = []
+        for row in range(int(self.max_x / self.grid_spacing + 1)):
+            for col in range(int(self.max_y / self.grid_spacing + 1)):
+                nodes.append(Node(row, col, index=self.calculate_node_index(row, col)))
+        return nodes
+
+    def calculate_node_index(self, x: float, y: float) -> int:
+        """
+        Calculates the index of a node in the grid
+        :param x: x-coordinate of the node
+        :param y: y-coordinate of the node
+        :return: index of the node
+        """
+        # index = row_len * y + x
+        return int(((self.max_x / self.grid_spacing) + 1) * y + x)
+
+    ############################################################################
+    # PLOT
+
+    def plot_nodes(self) -> None:
+        """
+        Plots the nodes in the grid
+        """
+        for node in self.nodes:
+            self.ax.text(node.x * self.grid_spacing, node.y * self.grid_spacing, node.index, color="red", fontsize="8")
+
+    def plot(self):
+        self.ax.set_title(f"Node Grid\nGrid Spacing: {self.grid_spacing}")
+        self.ax.set_xlim(0, self.max_x + self.grid_spacing)
+        self.ax.set_ylim(0, self.max_y + self.grid_spacing)
+        self.ax.set_ylabel("Y")
+        self.ax.set_xlabel("X")
+        
+        self.plot_nodes()
+        plt.show()
+        plt.close()
+        
+    ############################################################################
---
+++ README.md
@@ -0,0 +1,4 @@
+## Homework 1
+* Visibility and Reduced Graphs
+![alt text](https://github.com/nosv1/Christopher_Seagraves_Unmanned_Systems/blob/master/image.jpg?raw=true)
+* Creating a Node.py class and using it in a script
